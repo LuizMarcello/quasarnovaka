b217b3c7e99b1b7d3e06a394617a74464d6563ac
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-09-27 18:06:00 -0300

    Rolândia:27.09.22_18:05hs

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -9,18 +9,88 @@
       </q-toolbar>
     </q-header>
 
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header>
+          <!-- Essential Links -->
+          Menu
+        </q-item-label>
+
+        <q-list bordered separator>
+          <q-item to="home" exact>
+            <q-item-section avatar>
+              <!-- <q-icon name="home" /> -->
+              <q-icon name="mdi-home" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Home</q-item-label>
+              <q-item-label caption>Voltar para a home</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item to="/dashboard" exact>
+            <q-item-section avatar>
+              <!-- <q-icon name="list_alt" /> -->
+              <q-icon name="mdi-handshake-outline" />
+            </q-item-section>
+            <q-item-section
+              ><q-item-label>Parceria</q-item-label>
+              <q-item-label caption>Seja uma revenda Bentley</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item to="/quemsomosnos" exact>
+            <q-item-section avatar>
+              <!-- <q-icon name="list_alt" /> -->
+              <q-icon name="mdi-signal" />
+            </q-item-section>
+            <q-item-section
+              ><q-item-label>Quem somos nós</q-item-label>
+              <q-item-label caption>Bentley Brasil</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item to="/form" exact>
+            <q-item-section avatar>
+              <q-icon name="list_alt" />
+            </q-item-section>
+            <q-item-section
+              ><q-item-label>Cadastrar revenda</q-item-label>
+              <q-item-label caption>Preencha o formulário</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item to="/listarevendas" exact>
+            <q-item-section avatar>
+              <q-icon name="list_alt" />
+            </q-item-section>
+            <q-item-section
+              ><q-item-label>Revendas cadastradas</q-item-label>
+              <q-item-label caption>Todas as revendas</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <EssentialLink
+            v-for="link in essentialLinks"
+            :key="link.title"
+            v-bind="link"
+          />
+        </q-list>
+      </q-list>
+    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
     <q-footer elevated>
-    <q-toolbar class="q-gutter-md">
-      <q-icon name="facebook"/>
-      <q-icon name="mdi-twitter"/>
-      <q-icon name="mdi-instagram" />
-      <q-icon name="mdi-linkedin" />
-      <q-toolbar-title>Internet via satélite</q-toolbar-title>
-    </q-toolbar>
-  </q-footer>
+      <q-toolbar class="q-gutter-md">
+        <q-icon name="facebook" />
+        <q-icon name="mdi-twitter" />
+        <q-icon name="mdi-instagram" />
+        <q-icon name="mdi-linkedin" />
+        <q-toolbar-title>Internet via satélite</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
