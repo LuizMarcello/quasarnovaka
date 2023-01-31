5f50eb5db8b14475a1c17652ccda3bdb07b7beec
luizmarcello <luizmarcello.vmo@hotmail.com>
2023-01-30 22:05:50 -0300

    Londrina:30.01.23_22:05hs

--- src/composables/UseApi.js
+++ src/composables/UseApi.js
@@ -2,30 +2,46 @@ import useSupabase from "src/boot/supabase";
 import useAuthUser from "./UseAuthUser";
 
 export default function useApi() {
-  const { supabase } = useSupabase();
-  const { user } = useAuthUser();
+  const {
+    supabase
+  } = useSupabase();
+  const {
+    user
+  } = useAuthUser();
 
   const list = async (table) => {
-    const { data, error } = await supabase.from(table).select("*");
+    const {
+      data,
+      error
+    } = await supabase.from(table).select("*");
 
     if (error) throw error;
     return data;
   };
 
   const getById = async (table, id) => {
-    const { data, error } = await supabase.from(table).select("*").eq("id", id);
+    const {
+      data,
+      error
+    } = await supabase.from(table).select("*").eq("id", id);
     if (error) throw error;
     return data[0];
   };
 
   const getByBrCode = async (table, bar_code) => {
-    const { data, error } = await supabase.from(table).select("*").eq("bar_code", bar_code);
+    const {
+      data,
+      error
+    } = await supabase.from(table).select("*").eq("bar_code", bar_code);
     if (error) throw error;
     return data[0];
   };
 
   const post = async (table, form) => {
-    const { data, error } = await supabase.from(table).insert({
+    const {
+      data,
+      error
+    } = await supabase.from(table).insert({
       ...form,
 
       /* Passando o valor do id do usuário logado no momento,
@@ -38,13 +54,14 @@ export default function useApi() {
   };
 
   const update = async (table, form) => {
-    const { data, error } = await supabase
+    const {
+      data,
+      error
+    } = await supabase
       .from(table)
-      .update([
-        {
-          ...form,
-        },
-      ])
+      .update([{
+        ...form,
+      }, ])
       .match({
         id: form.id,
       });
@@ -53,11 +70,68 @@ export default function useApi() {
   };
 
   const remove = async (table, id) => {
-    const { data, error } = await supabase.from(table).delete().match({
+    const {
+      data,
+      error
+    } = await supabase.from(table).delete().match({
       id,
     });
     if (error) throw error;
     return data;
+  };
+
+  const statusTotal = async (table) => {
+    const {
+      error,
+      data,
+      count
+    } = await supabase
+      .from(table)
+      .select("*", {
+        count: "exact"
+      });
+  };
+
+  const statusAtivo = async (table) => {
+    const {
+      error,
+      data,
+      count
+    } = await supabase
+      .from(table)
+      .select("*", {
+        count: "exact"
+      }).match({
+        status: 'Ativo'
+      }).gt('views', 1000)
+  };
+
+  const statusAguardando = async (table) => {
+    const {
+      error,
+      data,
+      count
+    } = await supabase
+      .from(table)
+      .select("*", {
+        count: "exact"
+      }).match({
+        status: 'Aguardando'
+      }).gt('views', 1000)
+  };
+
+  const statusInativo = async (table) => {
+    const {
+      error,
+      data,
+      count
+    } = await supabase
+      .from(table)
+      .select("*", {
+        count: "exact"
+      }).match({
+        status: 'Inativo'
+      }).gt('views', 1000)
   };
 
   return {
@@ -67,5 +141,9 @@ export default function useApi() {
     post,
     update,
     remove,
+    statusTotal,
+    statusAtivo,
+    statusAguardando,
+    statusInativo
   };
 }
--- src/pages/clientes/InicioClientes.vue
+++ src/pages/clientes/InicioClientes.vue
@@ -19,17 +19,19 @@
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        <div class="text-h5">67</div>
+        <div class="text-h5">{{ ativosTotal }}</div>
       </q-card-section>
     </q-card>
+
     <br />
+
     <q-card class="my-card text-white" style="background: green">
       <q-card-section class="">
         <div class="text-h6">Total ativos</div>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        <div class="text-h5">52</div>
+        <div class="text-h5">{{ ativosAtivos }}</div>
       </q-card-section>
     </q-card>
 
@@ -37,11 +39,23 @@
 
     <q-card class="my-card text-white" style="background: #0d084f">
       <q-card-section>
-        <div class="text-h6">Total desativados</div>
+        <div class="text-h6">Total Inativos</div>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        <div class="text-h5">15</div>
+        <div class="text-h5">{{ ativosInativos }}</div>
+      </q-card-section>
+    </q-card>
+
+    <br />
+
+    <q-card class="my-card text-white" style="background: #0d084f">
+      <q-card-section>
+        <div class="text-h6">Total Aguardando</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-h5">{{ ativosAguardando }}</div>
       </q-card-section>
     </q-card>
   </div>
@@ -53,27 +67,86 @@
 
 <script>
 /* "defineComponent": Porque é vuejs 3 */
-import { defineComponent, ref, onMounted, computed } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
+
+//import useSupabase from "src/boot/supabase";
+
+import useApi from "src/composables/UseApi";
 
 /* import router from "src/router"; */
 import { useRouter } from "vue-router";
 import { useQuasar } from "quasar";
 
 export default defineComponent({
-  name: "InicioClientes",
-
-  methods: {},
-
   setup() {
     const router = useRouter();
 
-    /* const table = "revendas"; */
+    //const { supabase } = useSupabase();
+
+    //const table = "clientes";
+
     const $q = useQuasar();
+
+    const { statusTotal, statusAguardando, statusAtivo, statusInativo } =
+      useApi();
+
+    const ativosTotal = ref(0);
+    const ativosAguardando = ref(0);
+    const ativosAtivos = ref(0);
+    const ativosInativos = ref(0);
 
     function linkClick(e, go) {
       e.preventDefault(); // we choose when we navigate
     }
-    return { linkClick };
+
+    const handleStatusTotal = async () => {
+      try {
+        ativosTotal.value = await statusTotal("clientes");
+      } catch (error) {
+        notifyError(error.message);
+      }
+    };
+
+    const handleStatusAguardando = async () => {
+      try {
+        ativosAguardando.value = await statusAguardando("clientes");
+      } catch (error) {
+        notifyError(error.message);
+      }
+    };
+
+    const handleStatusAtivos = async () => {
+      try {
+        ativosAtivos.value = await statusAtivo("clientes");
+      } catch (error) {
+        notifyError(error.message);
+      }
+    };
+
+    const handleStatusInativos = async () => {
+      try {
+        ativosInativos.value = await statusInativo("clientes");
+      } catch (error) {
+        notifyError(error.message);
+      }
+    };
+
+    /* Duvida aqui */
+    //ativosTotal.value = "???";
+    //ativosAguardando.value = "???";
+    //ativosAtivos.value = "???";
+    //ativosInativos.value = "???";
+
+    /* onMounted(() => {
+      handleStatus();
+    }); */
+
+    return {
+      handleStatusTotal,
+      handleStatusAguardando,
+      handleStatusAtivos,
+      handleStatusInativos,
+    };
   },
 });
 </script>
